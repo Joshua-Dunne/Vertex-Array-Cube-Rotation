4567c426ee425e95c2493d89980d6fd69f1b2671
Joshua Dunne <C00241588@itcarlow.ie>
2020-01-19 19:49:13 +0000

    Added Translations, Rotations and Scaling using Matrix3 library.

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -50,12 +50,274 @@ void Game::update()
 {
 	elapsed = clock.getElapsedTime();
 
+	Matrix3 usableMatrix;
+	int reverse{ 1 };
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)
+		|| sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+	{
+		reverse = -1; // go backwards on rotations if shift is held
+	}
+	else
+	{
+		reverse = 1;
+	}
+
+	// Rotating
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+	{
+
+		usableMatrix = usableMatrix.RotationZ(0.6 * reverse);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+	{
+		usableMatrix = usableMatrix.RotationX(0.6 * reverse);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+	{
+		usableMatrix = usableMatrix.RotationY(0.6 * reverse);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	// Scaling
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+	{
+
+		usableMatrix = usableMatrix.Scale(101.0, 101.0);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::G))
+	{
+
+		usableMatrix = usableMatrix.Scale(99.9, 99.9);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	// Translating
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+
+		usableMatrix = usableMatrix.Translate(0, -0.1);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+
+		usableMatrix = usableMatrix.Translate(0, 0.1);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+
+		usableMatrix = usableMatrix.Translate(0.1, 0);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+
+		usableMatrix = usableMatrix.Translate(-0.1, 0);
+
+		for (int index = 0; index < 8; index++)
+		{
+			points[index] = usableMatrix * points[index];
+		}
+
+		for (int index = 0; index < 24; index += 3)
+		{
+			if (index != 0)
+			{
+				vertices[index] = points[index / 3].getX();
+				vertices[index + 1] = points[index / 3].getY();
+				vertices[index + 2] = points[index / 3].getZ();
+			}
+			else
+			{
+				vertices[index] = points[index].getX();
+				vertices[index + 1] = points[index].getY();
+				vertices[index + 2] = points[index].getZ();
+			}
+		}
+	}
+
 	cout << "Update up" << endl;
 }
 
 void Game::render()
 {
 	cout << "Drawing" << endl;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
--- SFMLOpenGL/Game.h
+++ SFMLOpenGL/Game.h
@@ -40,6 +40,17 @@ private:
 						1.0f, 1.0f, -1.0f,
 						-1.0f, 1.0f, -1.0f };
 
+	Vector3 points[8] = {
+		{ vertices[0], vertices[1], vertices[2]},
+		{ vertices[3], vertices[4], vertices[5]},
+		{ vertices[6], vertices[7], vertices[8]},
+		{ vertices[9], vertices[10], vertices[11]},
+		{ vertices[12], vertices[13], vertices[14]},
+		{ vertices[15], vertices[16], vertices[17]},
+		{ vertices[18], vertices[19], vertices[20]},
+		{ vertices[21], vertices[22], vertices[23]},
+	};
+
 	// Colors for those vertices
 	float colors[24] = { 1.0f, 0.0f, 0.0f,
 	0.0f, 1.0f, 0.0f,
@@ -51,6 +62,17 @@ private:
 	0.0f, 0.0f, 1.0f };
 
 	// Index to be drawn
-	unsigned int vertex_index[36] = { 2, 3, 0, 0, 1, 2, 7, 6, 5, 5, 4, 7, 6, 7, 3, 3, 2, 6, 1, 0, 4, 4, 5, 1, 
-									  3, 7, 4, 4, 0, 3, 6, 2, 1, 1, 5, 6 };
+	unsigned int vertex_index[36] = { 
+		2, 3, 0, 
+		0, 1, 2, 
+		7, 6, 5, 
+		5, 4, 7, 
+		6, 7, 3, 
+		3, 2, 6, 
+		1, 0, 4, 
+		4, 5, 1, 
+		3, 7, 4, 
+		4, 0, 3, 
+		6, 2, 1, 
+		1, 5, 6 };
 };
